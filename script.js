596e029581ed230339e84382e142c1acae8fe427
Abhishek Kumar Sinha <165494965+AbhishekSinhaa17@users.noreply.github.com>
2025-11-06 17:30:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function validEmail(str) {
   //your JS code here.
+	str = str.trim();
+
+  if (!str) return false;
+
+  const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+  return emailPattern.test(str);
 }
 
 // Do not change the code below.
